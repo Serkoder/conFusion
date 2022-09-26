c629a5e26f5a19a0ef74b17c5925a91a8d6c5bd4
ThinhPhan <phandthinhreal@gmail.com>
2022-09-26 11:03:32 +0700

    npm part 1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+      if ($("#carouselButton").children("button").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("button").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("button").children("span").addClass('fa-play');
+      }
+      else if ($("#carouselButton").children("button").children("span").hasClass('fa-play')) {
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("button").children("span").removeClass('fa-play');
+        $("#carouselButton").children("button").children("span").addClass('fa-pause');
+      }
+    });
+  });
